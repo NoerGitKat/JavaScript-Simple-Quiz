f8598b15c8c234f05b762a9f9173aa3cc3ed3013
Noer <noerpaanakker@hotmail.com>
2017-05-25 10:57:43 +0200

    final touches to JS, finished CSS and HTML

--- js/quiz.js
+++ js/quiz.js
@@ -1,7 +1,40 @@
 function submitAnswers() {
-	var questions = 5;
+	var total = 5;
 	var score = 0;
 
 	//Get user input from q1-q5
-	var q1 = document.forms[".quiz"][]
+	var q1 = document.forms["quiz"]["q1"].value;
+	var q2 = document.forms["quiz"]["q2"].value;
+	var q3 = document.forms["quiz"]["q3"].value;
+	var q4 = document.forms["quiz"]["q4"].value;
+	var q5 = document.forms["quiz"]["q5"].value;
+	var arrQ = [q1, q2, q3, q4, q5];
+
+	//Question validation
+	for (var i = 0; i < arrQ.length; i++) {
+		if (arrQ[i] == null || arrQ[i] == "" || arrQ[i] == undefined) {
+			alert("You skipped question #" + (i + 1) + "!");
+			return false;
+		}
+	}
+
+	//Setting correct answers
+	var answers= ["d", "a", "c", "b", "d"];
+	for (var i = 0; i <answers.length; i++) {
+		if(arrQ[i] == answers[i]) {
+			alert("Question #" + (i + 1) + " is correct!");
+			score++;
+		} else {
+			alert("Question #" + (i + 1) + " is WRONGGGGGGGG!");
+		}
+	}
+
+	//Add HTML with JS to display score
+	var results = document.getElementById("results");
+	results.innerHTML = "<h3>You scored <span>" + score + "</span> out of <span>" + total + "</span>!";
+	
+	alert("You scored " + score + " out of " + total);
+
+	//No database so don't return
+	return false;
 }
